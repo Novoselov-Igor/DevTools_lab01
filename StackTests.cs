6fb8b8384165e8f1e360034277300f94d0f90427
Новоселов Игорь Д19-191-1 <114796666+Novoselov-Igor@users.noreply.github.com>
2022-10-10 18:55:33 +0400

    Add files via upload

--- StackTests.cs
+++ StackTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Globalization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Wintellect.PowerCollections.Tests
 {
@@ -10,7 +11,7 @@ namespace Wintellect.PowerCollections.Tests
     public class StackTests
     {
         [TestMethod]
-        public void Count_EmptyStack_Test()
+        public void CountMustBeZero_EmptyStack_Test() // Проверка значения Count в пустом стеке (должно быть ноль)
         {
             Stack<string> tests = new Stack<string>(1);
 
@@ -18,46 +19,54 @@ namespace Wintellect.PowerCollections.Tests
         }
 
         [TestMethod]
-        public void Count_NotEmptyStack_Test()
+        public void Count_NotEmptyStack_Test() // Проверка значения Count в заполненном стеке
         {
             Stack<int> tests = new Stack<int>(4);
 
             tests.Push(1);
             tests.Push(2);
 
-            Assert.AreEqual(2, tests.Count);
+            Assert.AreEqual(2, tests.Count); // Ожидается 2, потому что в стек передано 2 значения
         }
 
 
         [TestMethod]
-        public void Capacity_Test() 
+        public void Capacity_Test() // Проверка значения Capacity при создании стека
         {
             Stack<bool> tests = new Stack<bool>(12);
 
-            Assert.AreEqual(12, tests.Capacity);
+            Assert.AreEqual(12, tests.Capacity); // Ожидается 12, потому что в конструктор передан размер - 12
         }
 
         [TestMethod]
-        public void StackConstructor_WithoutParameters_Test() 
+        public void StackConstructor_WithoutParameters_Test() // Проверка конструктора стека в случае когда в него ничто не передается
         {
             Stack<int> tests = new Stack<int>();
+
+            Assert.AreEqual(0, tests.Capacity); // Ожидается 0, потому что в конструктор не было ничего передано
         }
 
         [TestMethod]
-        public void StackConstructor_WithParameters_Test()
+        public void StackConstructor_WithParameters_Test() // Проверка работы конструктора при передаче параметров
         {
-            Stack<bool> tests1 = new Stack<bool>(100);
+            Stack<bool> tests = new Stack<bool>(100);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void StackConstructor_WithNegativeParameters_Test()
+        public void StackConstructor_WithNegativeParameters_Test() // Проверка работы конструктора при передаче в него отрицательного значения
         {
-            Stack<int> tests2 = new Stack<int>(-1);
+            try
+            {
+                Stack<int> tests2 = new Stack<int>(-1);
+            }
+            catch (InvalidOperationException e)
+            {
+                Assert.AreEqual("Введено отрицательное значение", e.Message); // Ожидается Exception с сообщением - "Введено отрицательное значение"
+            }
         }
 
         [TestMethod]
-        public void PushMethod_Work_Test()
+        public void PushMethod_Work_Test() // Проверка работы метода Push
         {
             Stack<int> tests = new Stack<int>(10);
 
@@ -65,12 +74,12 @@ namespace Wintellect.PowerCollections.Tests
             tests.Push(2);
             tests.Push(5);
 
-            Assert.AreEqual(5, tests.Top());
+            Assert.AreEqual(5, tests.Top()); // Поскольку последним было добавлено число 5, то и извлечься должно оно
         }
 
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
-        public void PushMethod_Overflow_Test()
+        public void PushMethod_Overflow_Test() // Проверка метода Push при переполнении размера стека (в данном случае размер - 0)
         {
             Stack<int> tests = new Stack<int>();
 
@@ -79,7 +88,7 @@ namespace Wintellect.PowerCollections.Tests
         }
 
         [TestMethod]
-        public void PopMethod_Work_Test()
+        public void PopMethod_Work_Test() // Проверка работы метода Pop
         {
             Stack<int> tests = new Stack<int>(5);
 
@@ -87,14 +96,13 @@ namespace Wintellect.PowerCollections.Tests
             tests.Push(2);
             tests.Push(5);
 
-            Assert.AreEqual(3, tests.Count);
-            Assert.AreEqual(5, tests.Pop());
-            Assert.AreEqual(2, tests.Count);
+            Assert.AreEqual(5, tests.Pop()); // Ожидается число 5, потому что оно на вершине
+            Assert.AreEqual(2, tests.Pop()); // Ожидается число 2, потому что оно стал самым верхним
         }
 
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
-        public void PopMethod_EmptyStack_Test()
+        public void PopMethod_EmptyStack_Test() // Проверка работы метода Pop при пустом стеке
         {
             Stack<int> tests = new Stack<int>(5);
 
@@ -102,7 +110,7 @@ namespace Wintellect.PowerCollections.Tests
         }
 
         [TestMethod]
-        public void TopMethod_Work_Test()
+        public void TopMethod_Work_Test() // Проверка работы метода Top (Count не должен меняться в отличии от Pop метода)
         {
             Stack<int> tests = new Stack<int>(5);
 
@@ -117,12 +125,30 @@ namespace Wintellect.PowerCollections.Tests
 
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
-        public void TopMethod_EmptyStack_Test()
+        public void TopMethod_EmptyStack_Test() // Проверка работы метода Top при пустом стеке
         {
             Stack<int> tests = new Stack<int>(5);
 
             tests.Top();
         }
 
+        [TestMethod]
+        public void GetEnumerator_Work_Test() // Проверка работы GetEnumerator метода
+        {
+            Stack<int> tests = new Stack<int>(5);
+            int[] pushes = new int[] { 23, 34, 5, 10 };
+
+            foreach (int n in pushes)
+            {
+                tests.Push(n);
+            }
+
+            int i = pushes.Length - 1;
+            foreach (int num in tests)
+            {
+                Assert.AreEqual(pushes[i], num);
+                i--;
+            }
+        }
     }
 }
